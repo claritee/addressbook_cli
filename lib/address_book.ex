87579c9a01a420cebda8e258ba7984636668afaf
Claire Tran <c.tran83@gmail.com>
2018-07-08 19:52:10 +1000

    Adding more details to the options

--- lib/address_book.ex
+++ lib/address_book.ex
@@ -4,8 +4,11 @@ defmodule AddressBook.CLI do
   """
 
   @doc """
-  TODO
-
+  Parse a CSV of people and find information
+  $ ./address_book --file people.csv --find oldest
+  $ ./address_book --file people.csv --find city --name Lily
+  $ ./address_book --file people.csv --find name --city Melbourne
+  $ ./address_book --file people.csv --find total
   """
   def main(args \\ []) do
     args
@@ -22,18 +25,22 @@ defmodule AddressBook.CLI do
   end
 
   defp response(opts) do
-    people = opts[:file]
-    |> Path.expand("#{__DIR__}/../")
-    |> File.stream!
-    |> CSV.decode
-    |> Enum.map(&(to_person(&1)))
-    [first | list] = people
-    Map.get(first, :name)
+    # IO.inspect(opts) #[file: "people.csv", find: "oldest"]
+    people = 
+      opts[:file]
+      |> Path.expand("#{__DIR__}/../")
+      |> File.stream!
+      |> CSV.decode
+      |> Enum.map(&(to_person(&1)))
+      |> response(opts)
+  end
+
+  defp response(people, [file: file, find: find] = opts) do
+    find
   end
 
   defp to_person(person_tuple) do
-    person = elem(person_tuple, 1)
-    [name, age, city] = person
+    [name, age, city] = elem(person_tuple, 1)
     %{name: name, age: age, city: city}
   end
 end
