92edea3b1ea0e5b801fcaff69ba2e42ee9addb3b
Claire Tran <c.tran83@gmail.com>
2018-07-08 16:16:42 +1000

    Adding initial project

---
+++ lib/address_book.ex
@@ -0,0 +1,18 @@
+defmodule AddressBook do
+  @moduledoc """
+  Documentation for AddressBook.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AddressBook.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
